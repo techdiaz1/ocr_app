1a006e5a57a62623529c7e37ad1bb31875b624b0
techdiaz1 <92034508+techdiaz1@users.noreply.github.com>
2021-12-28 20:54:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pytesseract 
+from PIL import Image
+
+st.title("OPTICAL CHARACTER RECOGNITION (OCR)")
+st.text("Upload an image to extract text!")
+
+uploaded_file = st.sidebar.file_uploader("Choose an image",type = ["jpg","png","jpeg"])
+if uploaded_file is not None:
+  img = Image.open(uploaded_file)
+  st.image(img, caption = 'Uploaded Image',use_column_width = True) 
+  st.write("")
+  
+  if st.button('PREDICT'):
+    st.write("RESULT:")
+    output = pytesseract.image_to_string(img)
+    st.write(output)
